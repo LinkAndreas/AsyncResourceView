a1570a5f555f74f33adfb18ba2b02ebf48abaa63
Andreas Link <mail@linkandreas.de>
2022-03-06 09:25:56 +0100

    Set minimum platforms to iOS 15 and macOS 12

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "AsyncResourceView",
+    platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
         .library(
             name: "AsyncResourceView",
